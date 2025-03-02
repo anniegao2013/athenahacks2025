e583e82f1b06dacba8185653f54b53100fd8dc6f
Annie Gao <anniegao@Annies-MacBook-Air.local>
2025-03-01 16:16:53 -0800

    login page visual

--- login.py
+++ login.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.markdown("<h1 style='text-align: center; color: black;'>Welcome to ________! </h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center; color: black;'>Some text here perhaps? Information? </p>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: black;'>Username</h3>", unsafe_allow_html=True)
-#add option to input username here
+username = st.text_input("")
 st.markdown("<h3 style='text-align: center; color: black;'>Password</h3>", unsafe_allow_html=True)
-#add option to input password here
+password = st.text_input(" ", type="password")
+login = st.button("Login")
